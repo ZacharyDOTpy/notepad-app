28f980b64072e4db79543597b22ab0054043a1f3
Zach <zach.Brewer.1995@gmail.com>
2024-02-04 14:51:32 -0500

    refactored server.js

--- server.js
+++ server.js
@@ -1,29 +1,22 @@
-const fs = require('fs');
 const express = require('express');
 const path = require('path');
-
-const uuid = require('./helpers/uuid');
+const api = require('./routes/index');
 
 const PORT = process.env.PORT ?? 3001;
 
 const app = express();
-
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
 app.use(express.static('public'));
+app.use('/api', api);
 
-app.get('/', (req, res) =>
+app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'views/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'views/index.html'))
+  res.sendFile(path.join(__dirname, 'views/notes.html'))
 );
-
-const api = require('./routes/index');
-app.use('/api', api);
 
 app.listen(PORT, () =>
   console.log(`Listening at http://localhost:${PORT}`)
